eea010ae9d76f60b129d7a1a8c154e2e036ab77b
marsar <marsar24@terpmail.umd.edu>
2023-09-21 21:34:37 +0530

    driver setup fix

--- driverIMX519Setup.sh
+++ driverIMX519Setup.sh
@@ -3,6 +3,8 @@
 wget -o install_pivariety_pkgs.sh https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/install_script/install_pivariety_pkgs.sh
 
 # Make executable
+rm install_pivariety_pkgs.sh
+mv install_pivariety_pkgs.sh.1 install_pivariety_pkgs.sh # Some bug which installs content in ...sh.1 file instead of ...sh
 chmod +rwx install_pivariety_pkgs.sh
 
 # Update Raspberry Pi
@@ -11,13 +13,12 @@ sudo apt update
 # Install libcamera and IMX519 drivers
 ./install_pivariety_pkgs.sh -p libcamera_dev
 ./install_pivariety_pkgs.sh -p libcamera_apps
-./install_pivariety_pkgs.sh -p imx519_kernel_driver
+./install_pivariety_pkgs.sh -p imx519_kernel_driver # don't install imx519_kernel_slow_...
 
-# Autofocus Code Repo
+# Clone Autofocus Code Repo
 git clone https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver.git
 
-
 # Update /boot/config.txt # To avoid buffer memory issues
-echo "dtoverlay=vc4-kms-v3d,cma-128" >> /boot/config.txt
+sudo echo "dtoverlay=vc4-kms-v3d,cma-128" >> /boot/config.txt
 
 # NOTE: LEGACY CAMERA SUPPORT needs to updated from raspi-config > Interface Options > Legacy Camera > Enable
