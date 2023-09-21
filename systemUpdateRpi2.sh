04fbc7c05aec0ab6d9a796948648f4478739ef03
marsar <marsar24@terpmail.umd.edu>
2023-09-21 21:29:03 +0530

    OpenCV Lite Version RPi2 Headless Bullseye

--- systemUpdateRpi2.sh
+++ systemUpdateRpi2.sh
@@ -1,7 +1,7 @@
 # This script adds a stable mirror to update the RPi2 on bullseye os
 
 # Edit sources.list
-sed -i '1,3 s/^/#/' # Comment out raspberry pi mirror
+sed -i '1,3 s/^/#/' # Comment out official raspberry pi mirror, it's broken most times.
 echo "deb http://raspbian.mirror.uk.sargasso.net/raspbian/ bullseye main contrib non-free rpi" >> /etc/apt/sources.list
 
 sudo apt update
